37139350aaf717c01e6dceb0657816392e66b612
Yakoob1884 <vivja.aty@gmail.com>
2026-09-10 13:13:30 +0200

    Added the db connection in our main.go and an internal folder for db. There is a error with our internal imported module

--- go-services/cmd/search-api/main.go
+++ go-services/cmd/search-api/main.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "log"
     "net/http"
+
+    "github.com/JOMS-inc/JOMSearch/go-services/internal/db"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    dbPath = "../db/whoknows.db"
+
+    conn, err := db.Open(dbPath)
+    if err != nill {
+        log.Fatalf("failed to open db: %v", err)
+    }
+
+    defer conn.Close()
+
+    mux := hhtp.NewServeMux()
+    mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request){
+        w.Write([]byte("ok"))
+    })
+
+    log.Println("listening on :8080")
+    log.Fatal(http.ListenAndServe(":8080", mux))
 }
